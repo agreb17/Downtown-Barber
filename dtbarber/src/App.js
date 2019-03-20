b09e0f4c1ff2f029ae3026fa38977f137343cc66
Anthony Greb <agreb255@gmail.com>
2019-03-20 19:29:07 -0400

    end of day

--- dtbarber/src/App.js
+++ dtbarber/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
 import { Route } from "react-router-dom";
-import HomeView from "";
+import HomeView from "./Views/homepage";
 import './App.css';
 
 const AppContainer = styled.div`
@@ -10,11 +10,16 @@ height: ;
 `;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
   render() {
     return (
       
         <AppContainer>
-          <Route exact path="/" render={props => <HomeView {...props} />} />
+          <Route exact path="/" render={ <HomeView /> } />
         </AppContainer>
       
     );
